0c26088fb8ca15c41170f03869953d4883966ce2
Averi <averi.sherman@gmail.com>
2022-10-17 18:34:02 -0400

    updated homeroutes, index, commentsroutes,

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // localhost:3001/api/comments
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newPost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const newComment = await Comment.create({
+        comment:req.body.content,
+        user_id: req.session.user_id,
+        post_id:req.body.post_id,
     });
 
-    res.status(200).json(newPost);
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
   }
