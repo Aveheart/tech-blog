26e4420f07a95a475d4f5778c0cfac854e4e3303
Averi <averi.sherman@gmail.com>
2022-10-19 23:02:06 -0400

    added formhandler for posting comments in post.js

---
+++ public/js/Post.js
@@ -0,0 +1,47 @@
+// const userComment = document.querySelector('.addComment');
+
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const userComment = document.querySelector('.addComment').value.trim();
+  
+    if (content) {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace(`/post/${post_id}`);
+      } else {
+        alert('Unable to post comment');
+      }
+    }
+  };
+  
+//   const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
+  
+//       const response = await fetch(`/api/posts/${id}`, {
+//         method: 'DELETE',
+//       });
+  
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to delete post');
+//       }
+//     }
+//   };
+  
+//   document
+//     .querySelector('.new-post-form')
+//     .addEventListener('submit', newFormHandler);
+  
+//   document
+//     .querySelector('.post-list')
+//     .addEventListener('click', delButtonHandler);
