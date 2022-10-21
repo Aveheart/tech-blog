9af36ed7001a3bc6caea00bb4a58445ef0f63c21
Averi <averi.sherman@gmail.com>
2022-10-21 02:07:04 -0400

    tried everyhting to get edit to post edited data and it wont work

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,8 +1,10 @@
 console.log(document.location)
 
 const editForm = document.querySelector('.editPost');
-const postForm = document.querySelector('.postNew')
-
+const postForm = document.querySelector('.postNew');
+const list = document.querySelector('.postlist');
+const label = document.querySelector('.label');
+const buttons = document.querySelector('.buttons');
 // need function for edit post form
 
 const showEditForm = async (event) => {
@@ -10,7 +12,11 @@ const showEditForm = async (event) => {
   const displayForm = document.querySelector(`form[editForm="${formEdit}"]`);
   editForm.style.display= "block";
   displayForm.style.display= "block";
-
+  postForm.style.display="none";
+  list.style.display="none";
+  label.style.display="none";
+  buttons.style.display="none";
+console.log('clicked edit on')
 
 }
 const newPost = async (event) => {
@@ -56,10 +62,12 @@ const newPost = async (event) => {
   // update a post
   const updatePost = async (event) => {
     if (event.target.hasAttribute('editDataId')) {
+      event.preventDefault();
+      console.log('clicked edit')
       
       const id = event.target.getAttribute('editDataId');
-      const name = event.target.getAttribute(`#editPostName${id}`).value.trim();
-      const description = event.target.getAttribute(`#editDataDesc${id}`).value.trim();
+      const name = event.target.querySelector(`#editPostName${id}`).value.trim();
+      const description = event.target.querySelector(`#editpostText${id}`).value.trim();
   
       const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
@@ -92,6 +100,11 @@ const newPost = async (event) => {
     .querySelector('.postEditbtn')
     .addEventListener('click', updatePost);
 
-    document
-    .querySelector('.editBtn')
-    .addEventListener('click', showEditForm);
+    // click edit button to show edit form
+    const editBtns = document.querySelectorAll('.editBtn');
+    editBtns.forEach((btn) => {
+      btn.addEventListener('click', showEditForm);
+    });
+    // document
+    // .querySelector('.editBtn')
+    // .addEventListener('click', showEditForm);
